e230dbdbe5b55959cc5b9b08427ff94d0449df22
Kirthana Sukumaran <kirthanaa07@gmail.com>
2023-08-28 13:36:38 -0500

    Added LOGO to the app

--- components/NavBarAuth.js
+++ components/NavBarAuth.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import Link from 'next/link';
 import {
-  Navbar, Container, Nav, Button,
+  Image, Navbar, Container, Nav, Button,
 } from 'react-bootstrap';
 import { signOut } from '../utils/auth';
 
@@ -10,13 +10,13 @@ export default function NavBarAuth() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
+        <Image src="/LOGO.png" alt="LOGO" />
         <Link passHref href="/">
           <Navbar.Brand>Profile</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto d-flex flex-grow-1 justify-content-between">
-            {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
             <Link passHref href="/credentials/new">
               <Nav.Link>Add Credential</Nav.Link>
             </Link>
